49efa153ed66a985e9c6d87973b4e8e606c3b1d3
Carl <carl@gmail.com>
2021-02-25 20:38:52 +0800

    added cowsay

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-#!/usr/bin/env node
+var balloon = require("./lib/balloon");
+var cows = require("./lib/cows");
+var faces = require("./lib/faces");
 
-console.log('simon says')
+exports.say = function (options) {
+    return doIt(options, true);
+};
+
+exports.think = function (options) {
+    return doIt(options, false);
+};
+
+exports.list = cows.list;
+
+function doIt (options, sayAloud) {
+    var cowFile;
+
+    if (options.r) {
+        var cowsList = cows.listSync();
+        cowFile = cowsList[Math.floor(Math.random() * cowsList.length)];
+    } else {
+        cowFile = options.f || "default";
+    }
+
+    var cow = cows.get(cowFile);
+    var face = faces(options);
+    face.thoughts = sayAloud ? "\\" : "o";
+
+    var action = sayAloud ? "say" : "think";
+    return balloon[action](options.text || options._.join(" "), options.n ? null : options.W) + "\n" + cow(face);
+}
+
+console.log('simon says anything goes')
